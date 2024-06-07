7b93917f60a1d342f7657ac485ff26d8823fc292
chaitanya <rayaproluchaitanya@gmail.com>
2024-06-07 07:45:07 +0530

    Initial File Commit

---
+++ src/test/java/demopackage/demotest2.java
@@ -0,0 +1,22 @@
+package demopackage;
+
+import org.testng.annotations.Test;
+
+public class demotest2 {
+	
+	@Test
+	public void test1() {
+		System.out.println("This is demo test2");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("This is demo test2");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("This is demo test2");
+	}
+
+}
